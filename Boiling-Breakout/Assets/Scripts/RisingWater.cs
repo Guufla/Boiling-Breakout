161619823bd8a2f4d7b53e3cb2466313edc60486
PinkJacketKid <75147007+PinkJacketKid@users.noreply.github.com>
2024-06-22 11:02:39 -0400

    Edited script

--- Boiling-Breakout/Assets/Scripts/RisingWater.cs
+++ Boiling-Breakout/Assets/Scripts/RisingWater.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RisingWater : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        SceneManager.LoadScene("RisingWaterTester");
     }
 
     // Update is called once per frame
